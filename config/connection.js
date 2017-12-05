6c3b6a170a3d8590321de5da0bbd25a4c7a273d4
Aaron Silverstein <adsilvercomp@Aarons-MacBook-Pro.local>
2017-12-05 18:05:06 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,19 @@
 var mysql = require("mysql");
 var connection
 
-  if(process.env.JAWSDB_URL){
-    connection= mysql.createConnection(process.env.JAWSDB_URL); 
-  }else{
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
   connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "Aaron",
-  password: "password",
-  database: "burgers_db"
-});
-
+    port: 3306,
+    host: "localhost",
+    user: "Aaron",
+    password: "password",
+    database: "burgers_db"
+  });
+}
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
