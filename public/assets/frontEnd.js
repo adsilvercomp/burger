8a6b5b4f6c65d42b834b8bcd7d2d31bc41f3edb4
Aaron Silverstein <adsilvercomp@Aarons-MBP.fios-router.home>
2017-12-05 10:18:44 -0500

    update

--- public/assets/frontEnd.js
+++ public/assets/frontEnd.js
@@ -6,14 +6,12 @@ $(".eat").on("click", function (event) {
     alert("This is working");
     var id = $(this).data("id");
     var status = $(this).data("devoured");
+    var name = $(this).data("burger_name");
     var eatState = {
         devoured: status
     };
-    if(status == true){
-        $(list1).remove(id);
-        $(list2).append(id);
-        console.log("bebimbop");
-    }
+    console.log(event);
+   
 
 
     /////////////////////////////////
@@ -24,7 +22,17 @@ $(".eat").on("click", function (event) {
     }).then(function () {
         alert("it is working");
         console.log("changed the status of devoured to", status);
+       
+        console.log("bebimbop");
+        $("#list2").append(id);
+        $("#list2").append(name);
+        // $("#list2").append(eatState);
+        $("#list1").detach(id);
+        $("#list1").detach(name);
         location.reload();
+        
+            
+        
     });
 });
 
